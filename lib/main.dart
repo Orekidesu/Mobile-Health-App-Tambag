277ea056cb27ebd5ba0cad4eeab8f5d56a9679ee
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2023-11-29 23:33:48 +0800

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Dashboard(),
+      home: const Login(),
     );
   }
 }
