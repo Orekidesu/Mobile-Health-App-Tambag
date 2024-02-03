5918cb6806ff1055778e45b0e2d4de4439d1061c
Orekidesu <cmcodog10@gmail.com>
2024-02-03 16:01:28 +0800

    Added PDF feature
    - Ready for testing

--- lib/model/patient_info.dart
+++ lib/model/patient_info.dart
@@ -1,1 +1,13 @@
-class patientInfo {}
+class PatientInfo {
+  final Future<Map<String, dynamic>> patientData;
+  final Future<List<Map<String, dynamic>>> medications;
+  final Future<List<Map<String, dynamic>>> medicationInteractions;
+  final Future<Map<String, Map<String, String>>> processedMedications;
+
+  const PatientInfo({
+    required this.patientData,
+    required this.medications,
+    required this.medicationInteractions,
+    required this.processedMedications,
+  });
+}
