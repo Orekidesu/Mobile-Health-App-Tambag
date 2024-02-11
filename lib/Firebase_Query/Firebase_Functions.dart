c89fdc98b4674eb22821c247fa8a7d21283d73e2
Orekidesu <cmcodog10@gmail.com>
2024-02-11 22:22:08 +0800

    Update Firebase_Functions.dart

--- lib/Firebase_Query/Firebase_Functions.dart
+++ lib/Firebase_Query/Firebase_Functions.dart
@@ -151,35 +151,6 @@ class DataService {
 }
 
 class PdfTableMap {
-  // Future<List<Map<String, dynamic>>> clientMedicationSummary(
-  //     String baranggay) async {
-  //   final QuerySnapshot<Map<String, dynamic>> patientSnapshot =
-  //       await FirebaseFirestore.instance
-  //           .collection('patients')
-  //           .where('address', isEqualTo: baranggay)
-  //           .get();
-
-  //   final List<Map<String, dynamic>> medicationQuantities = [];
-
-  //   for (final QueryDocumentSnapshot<Map<String, dynamic>> patientDoc
-  //       in patientSnapshot.docs) {
-  //     final QuerySnapshot<Map<String, dynamic>> medicationSnapshot =
-  //         await patientDoc.reference.collection('medications').get();
-
-  //     for (final QueryDocumentSnapshot<Map<String, dynamic>> medDoc
-  //         in medicationSnapshot.docs) {
-  //       final String medName = medDoc.data()['med_name'] as String;
-  //       final String medQuan = medDoc.data()['med_quan'].toString();
-
-  //       medicationQuantities.add({
-  //         'med_name': medName,
-  //         'med_quan': medQuan,
-  //       });
-  //     }
-  //   }
-  //   print(medicationQuantities);
-  //   return medicationQuantities;
-  // }
   Future<List<Map<String, dynamic>>> clientMedicationSummary(
       String baranggay) async {
     final QuerySnapshot<Map<String, dynamic>> patientSnapshot =
