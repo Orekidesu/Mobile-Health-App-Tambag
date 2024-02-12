096b96c460cfeff3ec4f666902891a05c6c4f8dd
Kyle Anthony Nierras <kyleanthony47@gmail.com>
2024-02-13 07:18:59 +0800

    Text Change

--- lib/Screen/Tracker.dart
+++ lib/Screen/Tracker.dart
@@ -124,24 +124,24 @@ class _TrackerState extends State<Tracker> {
                                 mainAxisAlignment: MainAxisAlignment.start,
                                 children: [
                                   CustomTextWidget(
-                                    text1: 'NAME:',
+                                    text1: 'Name:',
                                     text2: patientInfo['name'] ?? 'N/A',
                                   ),
                                   CustomTextWidget(
-                                    text1: 'AGE:',
+                                    text1: 'Age:',
                                     text2:
                                         patientInfo['age']?.toString() ?? 'N/A',
                                   ),
                                   CustomTextWidget(
-                                    text1: 'ADDRESS:',
+                                    text1: 'Address:',
                                     text2: patientInfo['address'] ?? 'N/A',
                                   ),
                                   CustomTextWidget(
-                                    text1: 'PHYSICIAN:',
+                                    text1: 'Physician:',
                                     text2: patientInfo['physician'] ?? 'N/A',
                                   ),
                                   CustomTextWidget(
-                                    text1: 'CONTACT NO.:',
+                                    text1: 'Mobile:',
                                     text2:
                                         patientInfo['contact_number'] ?? 'N/A',
                                   ),
